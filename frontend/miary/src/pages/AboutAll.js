98f4fcfe700881da0328f664a45eb51ab8820a3c
shun <vytngms@gmail.com>
2022-06-15 15:51:46 +0900

    no message

---
+++ frontend/miary/src/pages/AboutAll.js
@@ -0,0 +1,7 @@
+/**
+ * @author Shun
+ * @email vytngms@gmail.com
+ * @create date 2022-06-15 15:48:53
+ * @modify date 2022-06-15 15:48:59
+ * @desc [전체 매뉴 페이지]
+ */
---
+++ frontend/miary/src/pages/Setting.js
@@ -0,0 +1,7 @@
+/**
+ * @author Shun
+ * @email vytngms@gmail.com
+ * @create date 2022-06-15 15:48:39
+ * @modify date 2022-06-15 15:48:48
+ * @desc [관리 페이지]
+ */
