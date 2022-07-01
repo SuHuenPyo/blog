c2bb7042e4d4653231727a06236a8bb9f0b635aa
leede <cjsakfn2@gmail.com>
2022-07-01 22:40:04 +0900

    MIARY-40 [Front] 관리(서식)컨텐트 영역 디자인 생성

--- frontend/miary/src/pages/Management.js
+++ frontend/miary/src/pages/Management.js
@@ -3,6 +3,7 @@ import { Header } from '../components/Header/Header'
 import { Account } from '../components/Management/Account'
 import { Profile } from '../components/Management/Profile'
 import { Post } from '../components/Management/Post'
+import { Form } from '../components/Management/Form'
 import { Left } from '../components/Management/Left'
 import Style from '../assets/css/Management.module.css'
 
@@ -14,7 +15,7 @@ export const Management = () => {
         <Left></Left>
       </div>
       <div className={Style.Accountside}>
-        <Post></Post>
+        <Form></Form>
       </div>
     </div>
   )
