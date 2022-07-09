59760ee3b05ed5744196985fb6c70e2bad08c192
ejbaek <909back@naver.com>
2022-07-09 10:19:37 +0900

    [22/07/09] 커밋

--- backend/apis/profile/profile.ctrl.js
+++ backend/apis/profile/profile.ctrl.js
@@ -4,3 +4,25 @@ const logger = require("../../utils/winston");
 const RegexHelper = require("../../utils/RegexHelper");
 const { deleteImg } = require("../../utils/multer");
 
+const full = async(req, res, next) => {
+    logger.info(`[GET /profile/full] ${req.ip} is access`);
+
+    const id = parseInt(req.query.id,10);
+    let dbcon = null;
+    let json = null;
+
+    try {
+     dbcon = await pool.getConnection(async (conn) => conn);
+
+     const sql = 'SELECT userId,name,email,intro,image FROM members WHERE userId = ? ';
+
+     const [result] = await dbcon.query(sql,id);
+
+     
+
+    } catch (err) {
+
+    } finally {
+
+    }
+}
--- backend/apis/profile/profile.spec.js
+++ backend/apis/profile/profile.spec.js
@@ -1,3 +1,12 @@
 const request = require("supertest");
 const should = require("should");
 const app = require("../../app");
+
+
+describe("[GET /profile/full]",()=>{
+    describe("성공시", () => {
+        it("유저의 정보를 반환한다.",(done)=>{
+            
+        })
+    })
+})
