81771478f327a1e65eca114d7954add2b77e0389
shun <vytngms@gmail.com>
2022-08-22 22:35:50 +0900

    MIARY-42 [Backend] 이메일 인증 완료

--- backend/apis/mail/index.js
+++ backend/apis/mail/index.js
@@ -2,7 +2,7 @@
  * @author Shun
  * @email vytngms@gmail.com
  * @create date 2022-08-18 23:11:04
- * @modify date 2022-08-18 23:15:18
+ * @modify date 2022-08-22 22:35:11
  * @desc [description]
  */
 const express = require("express");
@@ -10,6 +10,7 @@ const router = express.Router();
 
 const {
   AuthCode,
+  VerifyCode
 
 } = require("./mail.ctrl");
 
@@ -36,4 +37,44 @@ const {
  *       400:
  *        description: 이메일 전송 실패
  */
- router.get("/mail", AuthCode);
+ router.get("/", AuthCode);
+
+
+ /**
+ * @swagger
+ *  /mail:
+ *    post:
+ *      tags:
+ *        - Mail
+ *      description: 인증번호 인증하기
+ *      parameters:
+ *        - in: body
+ *          name: authCode Info
+ *          description: The user to create.
+ *          schema:
+ *            type: object
+ *            properties:
+ *              authCode:
+ *               type: string
+ *      responses:
+ *         201:
+ *          description: 글 추가 성공
+ *         400:
+ *           description: 유효성 검사 실패
+ */
+ router.post("/", VerifyCode);
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+ module.exports = router;
--- backend/apis/mail/mail.ctrl.js
+++ backend/apis/mail/mail.ctrl.js
@@ -4,26 +4,29 @@ const nodemailer = require('nodemailer');
 const pool = require("../../utils/pool");
 const {AUTH_MAIL} = require('../../configs/_config.json');
 
-const AuthCode = async (req, res, next) => {
-  logger.info(`[GET /Mail] ${req.ip} is access`);
+//메일 인증번호 전송
+const AuthCode = async(req, res, next) => {
+  logger.info(`[GET /Mail] AuthCode ${req.ip} is access`);
   
   let dbcon = null;
-  let json = null;
   let RandAuthCode = null;
+  let transporter = null;
 
   try {
+
+    
     dbcon = await pool.getConnection(async (conn) => conn);
 
-    const {email} = req.body.email?.trim();
+    const email = req.query.email?.trim();
 
-    let RandAuthCode = await((digit)=>{return Math.random().toString(36).slice(2).substring(0,digit)})(6); // 나중에 helper로 따로 빼기
+    RandAuthCode = await((digit)=>{return Math.random().toString(36).slice(2).substring(0,digit)})(6); // 나중에 helper로 따로 빼기
 
     //메일 객체생성
-    let transporter = nodemailer.createTransport({
+    transporter = nodemailer.createTransport({
       service:'gmail',
       auth:{
           user: AUTH_MAIL.email,
-          pass: AUTH_MAIL.pass
+          pass: AUTH_MAIL.appPass
       }
     });
 
@@ -35,23 +38,69 @@ const AuthCode = async (req, res, next) => {
       html: '<h1>Miary Auth Code 입니다.</h1> <h2> 인증번호 : '+ RandAuthCode +' </h2> <h2>절대 타인에게 알리지 마세요.</h2>' 
     }
 
-    let [result] = await dbcon.query("SELECT COUNT(*) as cnt FROM miary.auth WHERE auth_email=?", email);
+    let sql = 'SELECT COUNT(*) as cnt FROM miary.auth WHERE auth_email = ?';
+    [result] = await dbcon.query(sql, [email]);
+
+    if(result[0].cnt > 0){
+      console.log("인증요청이 이미 있음 재요청함");
+
+      sql = 'DELETE FROM miary.auth WHERE auth_email=?';
+      [result] = await dbcon.query(sql, [email]);
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+      if(err) console.error(err);
+      else console.log("Email 전송성공  Auth Code : " + RandAuthCode);
+    });
 
 
+    console.log(email)
+    sql = 'INSERT INTO miary.auth(auth_email, auth_code, auth_done) VALUES(?, ?, ?)';
+    [result] = await dbcon.query( sql, [email, RandAuthCode, 0]);
 
-    const sql =
-      "SELECT b_id 'id', b_title 'title', b_banner 'banner', b_content 'content', m_id 'author', b_mdate 'date', b_hits 'hits', b_like 'like' FROM boards";
-
-    
-
-    json = result;
   } catch (err) {
     logger.error(err);
     console.log(err);
-    return res.status(400).send("Internal Server Error");
+    return res.status(500).send("서버 에러");
   } finally {
-    await dbcon.release();
+    if(dbcon) await dbcon.release();
+    if(transporter) await transporter.close();
   }
 
   return res.status(200).json(RandAuthCode);
 };
+
+//메일 인증번호 확인
+const VerifyCode = async( req, res, next)=>{
+  logger.info(`[POST /Mail] AuthCode ${req.ip} is access`);
+
+  let dbcon, result, transporter = null;
+  const authCode = req.body.authCode?.trim();
+  const email = req.body.email?.trim();
+
+  if(!authCode) return res.status(400).json("올바른 인증코드를 입력해주세요!");
+
+  try{
+    dbcon = await pool.getConnection(async (conn) => conn);
+    [result] = await dbcon.query("SELECT auth_code from auth WHERE auth_email = ?", [email]);
+    
+    if(result[0].auth_code == authCode){
+      await dbcon.query("UPDATE auth SET auth_done = 1 WHERE auth_email = ?", [email]);
+    }else{
+      return res.status(400).json("올바른 인증코드를 입력해주세요!");
+    }
+
+  }catch(err){
+    logger.error(err);
+    return res.status(500).json("internal error");
+
+  }finally{
+    if(dbcon) await dbcon.release();
+  }
+
+  logger.info(`[POST /Mail] AuthCode ${req.ip} 인증완료`);
+  return res.status(200).json("인증이 완료 되었습니다.");
+
+}
+
+module.exports = { AuthCode, VerifyCode };
