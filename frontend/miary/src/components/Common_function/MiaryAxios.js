3c866b8b07b621c66c2e4124452d2ba65045ec59
shun <vytngms@gmail.com>
2022-09-14 08:15:38 +0900

    Front Axios 모듈 기능 수정

--- frontend/miary/src/components/Common_function/MiaryAxios.js
+++ frontend/miary/src/components/Common_function/MiaryAxios.js
@@ -2,13 +2,14 @@
  * @author Shun
  * @email vytngms@gmail.com
  * @create date 2022-06-27 18:21:30
- * @modify date 2022-09-03 08:29:22
+ * @modify date 2022-09-14 03:16:22
  * @desc [Axios 요청을 위한 간편 모듈]
  */
 
 import axios from "axios"
 
-export const ServerUrl = "http://172.30.1.29:3300/"
+//export const ServerUrl = "http://172.30.1.29:3300/"
+export const ServerUrl = "http://localhost:3300/"
 
 export const MiaryGetAxios = async(url, log="", errLog="" , query={}) =>{
   let response = null;
@@ -19,28 +20,43 @@ export const MiaryGetAxios = async(url, log="", errLog="" , query={}) =>{
     console.log(log);
   }catch(err){
     console.error(errLog + '\n\n' + err);
+    return err;
   }finally{
     console.log("결과 : "+ url + " == "+ response.status);
+    
   }
   return response;
-  
 }
-
-export const MiaryPostAxios = async(url, form) =>{
+/**
+ * 
+ * @param {string} url  `Miary.ServerUrl + {사용하고자 할 api 라우터 주소}`
+ * @param {* & FormData} form `{data} - urlencoded 방식으로 전송하려면 {...} 입력. MultipartForm 방식 전송이라면 FormData 객체를 전송해준다.`
+ * @param {boolean} contentType `multipartForm 방식전송은 true or 1, urlencoded방식은 0 or false   - default 0(false);`
+ * @returns axios result
+ */
+export const MiaryPostAxios = async(url,form, contentType=0) =>{
   let response = null;
+
+
+  contentType = contentType ? 'multipart/form-data' : 'application/x-www-form-urlencoded;charset=UTF-8';
   try{
       response = await axios({
       method: 'post',
       url: url,
       data: form,
       header: {
-        'Content-Type': 'multipart/form-data',
+        'Content-Type': contentType,
       },
     });
-  }catch{
-    //handle error
-    console.error(response); 
+  }catch(err){
+    //console.error(err);
+    throw err;
+
+  }finally{
+    console.log("결과 : "+ url + " == "+ response.status);
+
   }
 
   return response;
+
 }
